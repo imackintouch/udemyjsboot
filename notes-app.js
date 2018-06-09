f83cbcb76d1fb4dd8bb6bb5fa9f57a9401f0bff4
imackintouch <imackintouch@gmail.com>
2018-06-09 14:08:12 -0400

    Commented out some code that was causing page to not function properly.

--- notes-app.js
+++ notes-app.js
@@ -22,9 +22,9 @@ const filters = {
 // console.log(userJSON)
 // localStorage.setItem('user', userJSON)
 
-const userJSON = localStorage.getItem('user')
-const user = JSON.parse(userJSON)
-console.log(`${user.name} is ${user.age}`)
+//const userJSON = localStorage.getItem('user')
+//const user = JSON.parse(userJSON)
+//console.log(`${user.name} is ${user.age}`)
 
 const renderNotes = function (notes, filters) {
     const filteredNotes = notes.filter(function(note) {
